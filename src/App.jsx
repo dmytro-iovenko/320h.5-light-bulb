8332c4a5d7dbdebc0d8a20712187ab5fb1cd9416
Dmytro Iovenko <iovenko@gmail.com>
2024-09-24 15:41:31 -0400

    add a brightness control feature

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,33 @@ import { useState } from "react";
 
 function App() {
   const [isBulbLit, setIsBulbLit] = useState(true);
+  const [brightness, setBrightness] = useState(100);
 
   const toggleSwitch = () => {
     new Audio(lightSwitchSound).play();
     setIsBulbLit((status) => !status);
   };
 
+  const handleBrightnessChange = (e) => {
+    setBrightness(e.target.value);
+  };
+
   const backgroundColor = isBulbLit ? "white" : "black";
   const color = isBulbLit ? "black" : "white";
 
   return (
-    <div style={{ backgroundColor, color }}>
+    <div
+      style={{
+        backgroundColor,
+        color,
+        filter: `brightness(${brightness}%)`,
+      }}>
       <h1>This room is {isBulbLit ? "LIT" : "DARK"}</h1>
       <img src={isBulbLit ? bulbOn : bulbOff} alt="light bulb" />
+      <div style={{ height: "auto" }}>
+        <label htmlFor="brightness">Brightness: {brightness}%</label>
+        <input id="brightness" type="range" min="0" max="100" value={brightness} onChange={handleBrightnessChange} />
+      </div>
       <div className={isBulbLit ? "switch" : "switch dark"} onClick={toggleSwitch}>
         <div className="switch-button"></div>
       </div>
